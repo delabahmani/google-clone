900f23549036fc0ca67a0eb1edb50dd3382b984c
Delara Bahmani <77635557+delabahmani@users.noreply.github.com>
2023-07-24 23:57:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const searchInput = document.querySelector("#search-input");
 
 
 searchInput.addEventListener("keydown", function(event) {
-    if(event.key === "Enter") {
+    if(event.key === "Enter" || event.key === "Go") {
         search();
     }
 });
@@ -14,4 +14,5 @@ function search() {
 
     window.location.href = "https://www.google.com/search?q=" + input + "&hl=en&sxsrf=AB5stBg7LEIeFMYtMiSLe1k1-LR1gQJskw%3A1690241466050&source=hp&ei=ugm_ZKs3sKqm1A_Ur5ygDg&iflsig=AD69kcEAAAAAZL8Xyn3XymorRXkx0PFCjhYORVLiCr-R&ved=0ahUKEwir_KmRwKiAAxUwlYkEHdQXB-QQ4dUDCAs&uact=5&oq=" + input + "&gs_lp=Egdnd3Mtd2l6IgVoZWxsbzIEECMYJzIKEC4YigUYsQMYQzIHEC4YigUYQzIHEC4YigUYQzIHEAAYigUYQzITEC4YigUYsQMYgwEYxwEY0QMYQzIHEC4YigUYQzIIEC4YgAQYsQMyCxAAGIAEGLEDGIMBMggQLhiABBixA0jnDFDACFj4C3ABeACQAQCYAXWgAa8DqgEDMC40uAEDyAEA-AEBqAIKwgIHECMY6gIYJ8ICBxAjGIoFGCfCAggQABiKBRiRAsICERAuGIAEGLEDGIMBGMcBGNEDwgIOEC4YgAQYsQMYxwEY0QPCAggQABiABBixA8ICDRAuGIoFGMcBGNEDGEPCAgUQLhiABMICCxAuGIAEGLEDGIMB&sclient=gws-wiz"
 }
+
 
